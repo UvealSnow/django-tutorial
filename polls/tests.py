79eb2facbfd9c04cfdbb0ccb67dbe17617c28aa4
Heredia <kevin.heredia@14325-sms.group.ava>
2020-02-07 14:48:44 +0100

    added tests for the QuestionIndexView

--- polls/tests.py
+++ polls/tests.py
@@ -8,6 +8,13 @@ from .models import Question
 # Run tests with the following command...
 # $ python manage.py test <app_name>
 
+def create_question(question_text, days):
+    """ 
+        Creates a question with the given text and days offset.
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
 class QuestionModelTest(TestCase):
     
     def test_was_published_recently_with_future_question(self):
@@ -36,3 +43,32 @@ class QuestionModelTest(TestCase):
         recent_question = Question(pub_date=time)
 
         self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_questions(self):
+        """ 
+            If no questions exist, an appropiate message is displayed
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available...")
+
+    def test_past_questions(self):
+        """ 
+            Questions with a pub_date in the past are displayed on the index page
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_questions(self):
+        """ 
+            Questions with pub_date in the future are not shown
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available...")
