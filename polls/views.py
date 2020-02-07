8ea032de3cea40b1f28a50ca2b79757db29e504c
Heredia <kevin.heredia@14325-sms.group.ava>
2020-02-07 13:51:41 +0100

    changed polls/views.py to use generic views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
+from django.views import generic
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
 
 # This import is no longer needed when render is imported
 # from django.template import loader
@@ -9,6 +10,44 @@ from .models import Question, Choice
 
 # Create your views here.
 
+class IndexView(generic.ListView):
+    # This would be useful if we hadn't changed the names of our templates
+    # By default ListView points to <app_name>/<model_name>_list.html
+    # template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    # This would be useful if we hadn't changed the names of our templates
+    # By default ListView points to <app_name>/<model_name>_detail.html
+    # template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You need to select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        # Returning an HttpResponseRedirect is required so that the user will not accidentally
+        # re-submit the form if he hits the back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+""" 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -38,22 +77,5 @@ def results(request, question_id):
 
     return render(request, 'polls/results.html', { 
         'question': question
-    })
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You need to select a choice"
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        
-        # Returning an HttpResponseRedirect is required so that the user will not accidentally
-        # re-submit the form if he hits the back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    }) 
+"""
