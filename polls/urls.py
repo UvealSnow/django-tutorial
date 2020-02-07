026f5fbb7193efb0d8f3419c15d14d6dbd9780fb
Heredia <kevin.heredia@14325-sms.group.ava>
2020-02-07 13:38:59 +0100

    ammend URLconf

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,16 @@ from . import views
 
 # app_name sets an application name space automatically
 app_name = 'polls'
+# This is the long way, just for learning how it works
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
